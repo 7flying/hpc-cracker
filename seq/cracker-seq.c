eeff625d1905f9e81d7be9ee13353f1d8041f1a8
Alchemy Meister <jesusesma@gmail.com>
2016-04-08 15:47:33 +0200

    keyspace calculator implemented.

--- seq/cracker-seq.c
+++ seq/cracker-seq.c
@@ -1,14 +1,28 @@
 #include "hasher.h"
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 
 
-void crack
-(char *to_crack, unsigned int lower, unsigned int higher, char *alphabet)
+unsigned long getKeySpace(unsigned int min, unsigned int max, char *alphabet)
+{
+	unsigned int i, alphabetlen = strlen(alphabet);
+	unsigned long size = 0;
+	for(i = max; i > min; i--) {
+		size += (unsigned long) pow(alphabetlen, i);
+	}
+
+	printf("Keyspace size: %lu\n", size);
+
+	return size;
+}
+
+void crack(unsigned int lower, unsigned int higher, char *alphabet)
 {
     unsigned int found = 0;
     int current = 0;
-    int keyspace = getkeyspace();
+    double keyspace = getKeySpace(lower, higher, alphabet);
     char *candidate = malloc(sizeof(higher) * (higher + 1));
 
     for (current = 0; current < keyspace; current++) {
@@ -16,4 +30,10 @@ void crack
     }
     
     free(candidate);
+}
+
+int main(int argc, char *argv[]) {
+	crack(4, 8, "012345678");
+
+	return 0;
 }
