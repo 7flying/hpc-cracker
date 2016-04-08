62a506ea9ac668bc98952b9357b8edb312b3e6cb
7flying <diez.f.irene@gmail.com>
2016-04-08 15:39:42 +0200

    Add getopt on cracker's main

--- seq/cracker-seq.c
+++ seq/cracker-seq.c
@@ -1,7 +1,9 @@
 #include "hasher.h"
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-
+int getkeyspace();
 
 void crack
 (char *to_crack, unsigned int lower, unsigned int higher, char *alphabet)
@@ -16,4 +18,28 @@ void crack
     }
     
     free(candidate);
+}
+
+
+int main(int argc, char **argv)
+{
+    int opt;
+    char *alph;
+    int high, low;
+    while ((opt = getopt(argc, argv, "h:l:a:")) != -1) {
+        switch (opt) {
+        case 'h':
+            high = atoi(optarg);
+            break;
+        case 'l':
+            low = atoi(optarg);
+            break;
+        case 'a':
+            alph = optarg;
+            break;
+        default:
+            abort();
+        }
+    }
+    printf("high: %d, low: %d, alphabet: %s\n", high, low, alph);
 }
